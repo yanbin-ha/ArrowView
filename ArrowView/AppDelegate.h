fbb9b88f9fba6949aef5ae8cbdb8b4f0b59e59f7
linaicai <linaicai2012@gmail.com>
2014-05-26 09:20:00 +0800

    Initial Commit

---
+++ ArrowView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArrowView
+//
+//  Created by linaicai on 14-5-26.
+//  Copyright (c) 2014年 LINAICAI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
